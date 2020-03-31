a4167f0b575b1fb5a77619287b85e59f250bf295
DPA <dobaipatakyattila@gmail.com>
2020-03-31 22:03:47 +0300

    Add sine-based float to the actors (looks cool)

--- src/Actor.cpp
+++ src/Actor.cpp
@@ -1,6 +1,9 @@
 #ifndef ACTOR_CPP
 #define ACTOR_CPP
 
+#include <ctime>
+#include <cmath>
+
 #include "Actor.h"
 
 Actor::Actor(Buffer* buf, Tile* tile) {
@@ -9,7 +12,7 @@ Actor::Actor(Buffer* buf, Tile* tile) {
 }
 
 void Actor::draw() {
-  buf->translate(tile->get_pos());
+  buf->translate(tile->get_pos() * (1.01 +  0.01 * sin(clock() / 100000.0)));
   Point2f angles = tile->get_angles();
   buf->rotate(angles.x, angles.y);
   buf->draw();
